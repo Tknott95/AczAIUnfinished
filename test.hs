9bd3931371c5813da7fc8a5a8ef45cc5fa17d38f
Trevor Knott <Tknott95@hotmail.com>
2023-08-12 08:58:12 -0600

    Acz, the AI. UNFINISHED - preLLMS

--- test.hs
+++
@@ -1,8 +0,0 @@
-import Data.Array.Accelerate              as A
-import Data.Array.Accelerate.LLVM.Native  as CPU
--- import Data.Array.Accelerate.LLVM.PTX     as GPU
-
-dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
-dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
-
-main = print $ CPU.run $ dotp (use xs) (use ys)
